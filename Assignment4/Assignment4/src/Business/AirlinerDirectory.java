f5633877e82428dfc7370980731625f925f705fe
jain.adit@northeastern.edu <aditijain2025@gmail.com>
2020-10-24 17:53:14 -0400

    ASS4Skeleton

---
+++ Assignment4/Assignment4/src/Business/AirlinerDirectory.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+import java.util.List;
+/**
+ *
+ * @author aditi
+ */
+public class AirlinerDirectory {
+    private List<Airliner> airlinerList;
+
+    public List<Airliner> getAirlinerList() {
+        return airlinerList;
+    }
+
+    public void setAirlinerList(List<Airliner> airlinerList) {
+        this.airlinerList = airlinerList;
+    }
+    
+    
+    
+    
+}
